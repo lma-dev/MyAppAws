8196ae7b3c6bd3745f103f25c2e9d92aa6fc5274
lma-dev <lwinmoeaung.it@gmail.com>
2024-11-16 13:11:09 +0900

    test ;og

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/test', function () {
+    Log::info('test');
     return "test";
 });
 
